85300f7aa929890beb66c0611652b0d2ff8e981d
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-04-15 11:53:53 +0200

    Ex21

--- CustomExceptionHandling/Program.cs
+++ CustomExceptionHandling/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace CustomExceptionHandling
+﻿using TotallySafeLib;
+
+namespace CustomExceptionHandling
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Øvelse 2.2 Inspektion og afprøvning
+            // Test GetValueAtPosition med tallene 1, 4, 9 og -2
+            // 9 og -2 giver IndexOutOfRangeExeption
+
+            // Øvelse 2.3 Håndtering af exception (try-catch)
+            // Øvelse 2.4 Inspektion og afprøvning af Divider og StringToInt metoden
+
+            try {
+                double divider = TotallySafe.Divider(2);
+                Console.WriteLine(divider);
+
+                // string s = null; //Test ArgumentNullException
+                int stringToInt = TotallySafe.StringToInt("1350");
+                Console.WriteLine(stringToInt);
+
+                int posValue = TotallySafe.GetValueAtPosition(-1);
+                Console.WriteLine(posValue);
+
+            }
+            catch(NegativeIndexOutOfRangeException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex) {
+                // Get the error message out the exception that has been thrown
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a valid number.");
+
+            }
+            catch (DivideByZeroException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex) {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Input string must contain numbers.");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ TotallySafeLib/TotallySafe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TotallySafeLib
+{
+    public class TotallySafe
+    {
+        public static double Divider(int number)
+        {
+            return 7 / number;
+        }
+        public static int StringToInt(string stringToConvert)
+        {
+            return int.Parse(stringToConvert);
+        }
+        public static int GetValueAtPosition(int positionInArray)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+
+            // Hvis input er negativt, throw new custom exception
+            if (positionInArray < 0) {
+                throw new NegativeIndexOutOfRangeException("MyCustomExceptionMessage: Parameter index cannot be negative.");
+            }
+
+            return intArray[positionInArray];
+        }
+    }
+
+    // Definér din egen brugerdefinerede Exception klasse (indeholder 3 constructors)
+    public class NegativeIndexOutOfRangeException : Exception
+    {
+        public NegativeIndexOutOfRangeException()
+        { 
+        }
+        public NegativeIndexOutOfRangeException(string message) : base(message)
+        {  
+        }
+        public NegativeIndexOutOfRangeException(string message, Exception inner) : base(message, inner)
+        {            
+        }
+    }
+}
